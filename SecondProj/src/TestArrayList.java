f9e52aca357a3d5a8b080029a9895c813ed2965d
Pikarashid <nurafiqahhusna@gmail.com>
2018-02-12 16:39:45 +0800

    New Modify

--- SecondProj/src/TestArrayList.java
+++ SecondProj/src/TestArrayList.java
@@ -1,9 +1,31 @@
+import java.util.ArrayList;
 
 public class TestArrayList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<String> al = new ArrayList<>();
+		
+		for (String str:args) {
+			al.add(str);
+		}
+		
+		System.out.println("Array list is empty:" + al.isEmpty());
+		System.out.println("Array list Size is:" + al.size());
+		
+		//al.add(new Integer(10));
+		//al.add(Boolean.TRUE);
+		System.out.println("Original : " + al);
+		al.remove("Two");
+		System.out.println("Now : " + al);
+		al.set(1, "New");
+		
+		System.out.println("Now : " + al);
+		
+		System.out.println("Contains four : " + al.contains("four"));
+		al.clear();
+		System.out.println("Now : " + al);
+		
 	}
 
 }
